1e3ad62a341f6cac2c6521dcbc8e971b2494b5dd
Marco Oliveira <marco.oliveira@agrosmart.com.br>
2021-03-31 00:46:13 -0300

    refactor password hash | add authentication method | add test to get_user/1

--- lib/salve/users.ex
+++ lib/salve/users.ex
@@ -1,7 +1,9 @@
 defmodule Salve.Users do
   alias Salve.Users.User
   alias Salve.Repo
-  alias Bcrypt
+  alias Argon2
+
+  import Ecto.Query, only: [from: 2]
 
   def create_user(%{} = attrs) do
     %User{}
@@ -24,4 +26,19 @@ defmodule Salve.Users do
   end
 
   def get_user!(id), do: Repo.get!(User, id)
+
+  def authenticate_user(email, password) do
+    query = from u in User, where: u.email == ^email
+    case Repo.one(query) do
+      nil ->
+        Argon2.no_user_verify()
+        {:error, :invalid_credentials}
+      user ->
+        if Argon2.verify_pass(password, user.password) do
+          {:ok, user}
+        else
+          {:error, :invalid_credentials}
+        end
+    end
+  end
 end
